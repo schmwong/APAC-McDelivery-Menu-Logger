8c1045cbc685a07a5de9475f04a30c98757e80c0
Paul Wong <79643071+schmwong@users.noreply.github.com>
2022-01-19 09:45:29 +0000

    added .py script for Singapore

---
+++ mcd-bs4-sg/CHANGELOG.md
@@ -0,0 +1,43 @@
+## Singapore Changelog
+
+- This is my first automated webscraping project.
+- Written in Python 3.8.2.
+-	Libraries:
+	-	**bs4** : Beautiful Soup 4
+	-	**pandas**
+	-	**datetime**
+	-	**requests**
+	-	**re** : Regular Expressions
+- Webpage URLs were manually added instead of extracting `href` values from the starting URL.
+
+---
+
+### 0.0.2 (2022-01-19)
+
+***New***
+
+1. Created scraper to get the live SGD to USD exchange rate from Xe.com.
+
+
+***Improved***
+
+1. Edited and expanded Dictionary:
+	1. New layout containing 9 fields: <br> { `Date`, `Day`, `Territory`, `Menu Item`, `Price (SGD)`, `Price (USD)`, `Category`, `Menu` }
+	2. Added 2 datetime fields: `Date` in *yyyy-mm-dd*, and `Day` in *ddd* format.
+	3. Added `Territory` field to enable filtering by Country / Territory in future projects.
+	4. Changed name of `Name` field to `Menu Item`.
+	5. Changed name of `Price` field to `Price (SGD)`.
+	6.  Added `Price (USD)` field, using the live exchange rate for currency conversion.
+
+2. Timestamp with `[yyyy-mm-dd HH:MM:SS]` format added as prefix to `.csv` filename.
+
+### 0.0.1 (2022-01-18)
+
+***Initial Development***
+
+1. Started with the initial draft on Jupyter Notebook.
+2. Set HTTP request headers.
+3. Changed CSS selector for inner loop of scraper to `"div.product-card"`.
+4. Tested parsers `lxml` and `html5lib` before deciding on `lxml`.
+5. Built Dictionary (i.e. associative array) containing 4 fields: `Name`, `Price`, `Category`, `Menu`.
+6. Tested DataFrame (i.e. table) construction and saving to `.csv` file.
---
+++ mcd-bs4-sg/mcd-bs4-sg.py
@@ -0,0 +1,98 @@
+import requests as r
+from bs4 import BeautifulSoup as BS
+import pandas as pd
+import datetime as dt
+import re
+
+
+current_date = dt.datetime.now()
+
+
+# Set headers to make HTTP request to seem to be from a normal browser
+my_headers = {
+    "Access-Control-Allow-Origin":"*",
+    "Access-Control-Allow-Methods": "GET",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Max-Age": "3600",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36", 
+    "Accept": "text/html,application/xhtml+xml,application/xml; q=0.9,image/webp,image/apng,*/*;q=0.8"
+}
+              
+session = r.Session()
+
+
+# --------------------------------------- #
+# Getting the Live Exchange Rate from XE  #
+# --------------------------------------- #
+
+# Getting the correct XE webpage (all elements)
+XE = BS(session.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=SGD&To=USD", headers=my_headers).content, "lxml")
+
+# Scraping the text from the selected element (CSS selector)
+# Extracting only the number from the text string and converting it to a float value (decimal number) 
+# findall() and select() methods return a list, indicate index [0] to extract the first element as a string value
+exchange_rate = float(re.findall(r"[-+]?(?:\d*\.\d+|\d+)", XE.select("p.result__BigRate-sc-1bsijpp-1.iGrAod")[0].text)[0])
+
+
+# ------------------------------------------- #
+# List of URLS of all Webpages to be Scraped  #
+# ------------------------------------------- #
+
+URL_list = [     # Regular Menu
+    "https://www.mcdelivery.com.sg/sg/browse/menu.html?daypartId=21&catId=29", # Promotion Meals 
+    "https://www.mcdelivery.com.sg/sg/browse/menu.html?daypartId=21&catId=63", # Chicken McCrispy®
+    "https://www.mcdelivery.com.sg/sg/browse/menu.html?daypartId=21&catId=66", # Sharing
+    "https://www.mcdelivery.com.sg/sg/browse/menu.html?daypartId=21&catId=30", # Ala Carte & Value Meals
+    "https://www.mcdelivery.com.sg/sg/browse/menu.html?daypartId=21&catId=31", # Sides
+    "https://www.mcdelivery.com.sg/sg/browse/menu.html?daypartId=21&catId=32", # Desserts
+    "https://www.mcdelivery.com.sg/sg/browse/menu.html?daypartId=21&catId=33", # Beverages
+    "https://www.mcdelivery.com.sg/sg/browse/menu.html?daypartId=21&catId=34", # For the Family
+                 # Breakfast Menu 
+    "https://www.mcdelivery.com.sg/sg/browse/menu.html?daypartId=22&catId=29", # Promotion Meals
+    "https://www.mcdelivery.com.sg/sg/browse/menu.html?daypartId=22&catId=30", # Breakfast & Value Meals
+    "https://www.mcdelivery.com.sg/sg/browse/menu.html?daypartId=22&catId=31", # Sides
+    "https://www.mcdelivery.com.sg/sg/browse/menu.html?daypartId=22&catId=32", # Desserts
+    "https://www.mcdelivery.com.sg/sg/browse/menu.html?daypartId=22&catId=33", # Beverages
+    "https://www.mcdelivery.com.sg/sg/browse/menu.html?daypartId=22&catId=34"  # For the Family
+]
+
+
+# -------------------------------------- #
+# Parsing the data into Dictionary List  #
+# -------------------------------------- #
+
+# Initialising the dictionary list object
+product_list = []
+
+
+# Outer loop iterates through list of webpages
+for url in URL_list:
+  response = session.get(url, headers=my_headers)
+  soup = BS(response.content, "lxml")
+    
+  # Inner loop iterates through elements on each webpage
+  for products in soup.select("div.product-card"):
+        product = {}
+        product["Date"] = current_date.strftime("%Y/%m/%d")
+        product["Day"] = current_date.strftime("%a")
+        product["Territory"] = "Singapore"
+        product["Menu Item"] = products.select("h5.product-title")[0].text
+        product["Price (SGD)"] = float((re.findall(r"[-+]?(?:\d*\.\d+|\d+)",products.select("span.starting-price")[0].text)[0]))
+        product["Price (USD)"] = round((product["Price (SGD)"] * exchange_rate), 2)
+        product["Category"] = soup.select("ol.breadcrumb > li.active")[0].text
+        product["Menu"] = soup.select("li.primary-menu-item.selected > a > span")[0].text
+        product_list.append(product)
+
+
+# ------------------------------------------------- #
+# Constructing the Dataframe and Saving it to File  #
+# ------------------------------------------------- #
+
+product_list_df = pd.DataFrame(product_list)
+
+print(product_list_df)
+
+# timestamp = str(current_date.strftime("[%Y-%m-%d %H:%M:%S]"))
+# product_list_df.to_csv(str(timestamp + " mcd-bs4-sg.csv"), encoding="utf-8")
+
+# Output filename format: "[YYYY-MM-DD hh:mm:ss] mcd-bs4-sg.csv"
