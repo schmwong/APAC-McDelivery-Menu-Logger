72cc52289471daa97d1c1e749fb5f1b4940cefb1
Paul Wong <79643071+schmwong@users.noreply.github.com>
2022-06-07 10:05:11 +0000

    enabled mkdir method to create new folder to hold scraped data files

--- mcd-scr-au/mcd/spiders/mcd-scr-au.py
+++ mcd-scr-au/mcd/spiders/mcd-scr-au.py
@@ -191,7 +191,7 @@ class McdScrAuSpider(scrapy.Spider):
 		output_dir = Path("./scraped-data")
 
 		# Create directory as required; won't raise an error if directory already exists
-		# output_dir.mkdir(parents=True, exist_ok=True)
+		output_dir.mkdir(parents=True, exist_ok=True)
 
 		product_list_df.to_csv((output_dir / output_file),
 				       float_format="%.2f", encoding="utf-8")
