860902d9a6f4e308a5dbb776b77bdc956d36ab5d
Paul Wong <smwong87@gmail.com>
2023-11-07 13:59:29 +0800

    Update CSS selector for Xe exchange rate

--- mcd-bs4-sg/mcd-bs4-sg.py
+++ mcd-bs4-sg/mcd-bs4-sg.py
@@ -40,7 +40,7 @@ try:
 	# Extracting only the number from the text string and converting it to a float value (decimal number)
 	# findall() and select() methods return a list, indicate index [0] to extract the first element as a string value
 	exchange_rate = float(re.findall(
-	    r"[-+]?(?:\d*\.\d+|\d+)", XE.select("p.result__BigRate-sc-1bsijpp-1.iGrAod")[0].text)[0])
+	    r"[-+]?(?:\d*\.\d+|\d+)", XE.select("p[class*=result__BigRate-sc-1bsijpp-1]")[0].text)[0])
 	
 	
 	# ------------------------------------------- #
