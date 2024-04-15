d4095e7e4cfc94480929af01e3e75b1a6349ad27
Paul Wong <smwong87@gmail.com>
2024-04-15 22:00:44 +0800

    Updated FX element selector for New Zealand

--- mcd-scr-nz/mcd/spiders/mcd-scr-nz.py
+++ mcd-scr-nz/mcd/spiders/mcd-scr-nz.py
@@ -1,5 +1,6 @@
 ''' ====================================================== ''''''
-| Menu data retrieved via POST request to UberEats API endpoint |
+|  Menu data scraped from script element on UberEats order page |
+|    (previously via POST request to UberEats API endpoint)     |
 '''''' ====================================================== '''
 
 # pip3 install scrapy
@@ -59,14 +60,22 @@ class McdScrNzSpider(scrapy.Spider):
 	def parse_fx(self, response):
 
 		try:
-			# exchange rate is stored in two parts, across two HTML elements
-			# parsing the CSS selectors return string values, which are concatenated,
-			# and the resulting string is stored as a float (decimal) value, 
-			# to be used in later mathematical operations
+			# exchange rate is stored in two parts, across two HTML elements (one in the parent, one in the child element)
+			# XPath selector locates child element then selects its immediate parent (because the child element has a constant class attribute value)
+			# parsing the xpath here returns the text values of the selected element and all its children,
+			# regex is used to extract only numerical portions of the parsed text,
+			# and the resulting string is stored as a float (decimal) value, to be used in later mathematical operations
 			exchange_rate = float(
-				response.css("p.result__BigRate-sc-1bsijpp-1.iGrAod::text").get() +
-				response.css("span.faded-digits::text").get()
+				re.findall(
+					r"[-+]?(?:\d*\.\d+|\d+)",
+					response.xpath("//span[contains(@class,'faded-digits')]/..//text()").get()
+				)[0]
 			)
+
+			print(
+				f"\n1 NZD = {exchange_rate} USD (1 USD = {1/exchange_rate} NZD) on {local_datetime.strftime('%A, %-d %B %Y')}"
+			)
+			print()
 	
 			
 			url = "https://www.ubereats.com/api/getStoreV1?localeCode=nz"
