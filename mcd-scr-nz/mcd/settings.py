a567bad831143a4d4d6b837e8b043618617b1905
Paul Wong <smwong87@gmail.com>
2024-04-18 12:01:39 +0800

    Added user agent for scrapy

--- mcd-scr-nz/mcd/settings.py
+++ mcd-scr-nz/mcd/settings.py
@@ -15,6 +15,7 @@ NEWSPIDER_MODULE = 'mcd.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'mcd_scr_nz (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
