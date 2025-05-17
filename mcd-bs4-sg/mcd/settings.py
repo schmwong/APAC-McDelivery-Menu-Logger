bf33a15a9faff8f05d9c84b050849803ab6a94f4
schmwong <smwong87@gmail.com>
2025-05-17 17:21:40 +0800

    update download handler to use HTTP2 protocol

--- mcd-bs4-sg/mcd/settings.py
+++ mcd-bs4-sg/mcd/settings.py
@@ -16,6 +16,10 @@ NEWSPIDER_MODULE = "mcd.spiders"
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36"
 
+DOWNLOAD_HANDLERS = {
+    "https": "scrapy.core.downloader.handlers.http2.H2DownloadHandler"
+}
+
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
